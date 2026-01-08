8775387858cf65e8153fb287d2ec0fe8a140166b
kai-done <kkwek@42mail.sutd.edu.sg>
2026-01-08 18:20:52 +0800

    Update get_next_line.h

--- Milestone 1/get_next_line/get_next_line.h
+++ Milestone 1/get_next_line/get_next_line.h
@@ -18,7 +18,14 @@
 # endif
 
 # include <fcntl.h>
+# include <stdint.h>
+# include <stddef.h>
 
 char	*get_next_line(int fd);
+char	*ft_strdup(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+size_t	ft_strlen(const char *c);
+char	*join_and_free(char *s1, char *s2);
 
 #endif 
