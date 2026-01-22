87810fc5923512228a917984a29b676a8b1e7ff7
kai-done <z.k.zaden@gmail.com>
2026-01-22 17:46:27 +0800

    Update get_next_line_utils.c

--- Milestone 1/get_next_line/get_next_line_utils.c
+++ Milestone 1/get_next_line/get_next_line_utils.c
@@ -17,82 +17,67 @@
 #include <stdint.h>
 #include "get_next_line.h"
 
-size_t	ft_strlen(const char *c)
+int	length(char *s1)
 {
-	size_t	num;
+	int	i;
 
-	num = 0;
-	while (c[num] != '\0')
-		num++;
-	return (num);
+	i = 0;
+	if (s1 == NULL)
+		return (0);
+	while (s1[i] != '\0')
+		i++;
+	return (i);
 }
 
-void	*ft_memcpy(void *dest, const void *src, size_t n)
+char	*duplicate(char *s1)
 {
-	unsigned char		*paste;
-	const unsigned char	*copy;
-	size_t				num;
+	int		i;
+	char	*s2;
 
-	paste = dest;
-	copy = src;
-	num = 0;
-	while (num < n)
+	i = 0;
+	if (s1 == NULL)
+		return (NULL);
+	s2 = malloc(length(s1) + 1);
+	if (s2 == NULL)
+		return (NULL);
+	while (s1[i] != '\0')
 	{
-		paste[num] = copy[num];
-		num++;
+		s2[i] = s1[i];
+		i++;
 	}
-	return (dest);
+	s2[i] = '\0';
+	return (s2);
 }
 
-char	*ft_strdup(const char *s)
+int	loop(char *s1, char *s2, int i)
 {
-	size_t	len;
-	char	*copy;
+	int		j;
 
-	if (s == NULL)
-		return (NULL);
-	len = ft_strlen(s) + 1;
-	copy = malloc(len);
-	if (copy == NULL)
-		return (NULL);
-	ft_memcpy(copy, s, len);
-	return (copy);
+	j = 0;
+	while (s1[j] != '\0')
+	{
+		s2[i] = s1[j];
+		i++;
+		j++;
+	}
+	return (i);
 }
 
-
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*join(char *s1, char *s2)
 {
 	char	*s3;
-	size_t	len;
-	size_t	num;
-	size_t	str1;
+	int		i;
 
-	num = 0;
-	str1= ft_strlen(s1);
-	len = str1 + ft_strlen(s2);
-	s3 = (char *)malloc(len + 1);
+	i = 0;
+	if (s1 == NULL)
+		s1 = duplicate("");
+	if (s1 == NULL || s2 == NULL)
+		return (free(s1), NULL);
+	s3 = malloc(length(s1) + length(s2) + 1);
 	if (s3 == NULL)
-		return (NULL);
-	while (s1[num] != '\0')
-	{
-		s3[num] = s1[num];
-		num++;
-	}
-	num = 0;
-	while (s2[num] != '\0')
-	{
-		s3[num + str1] = s2[num];
-		num++;
-	}
-	s3[len] = '\0';
-	return (s3);
-}
-
-char	*join_and_free(char *s1, char *s2)
-{
-	char	*result;
-	
-	result = ft_strjoin(s1, s2);
-	free(s1);
-	return (result);
+		return (free(s1), NULL);
+	i = loop(s1, s3, i);
+	i = loop(s2, s3, i);
+	s3[i] = '\0';
+	return (free(s1), s3);
 }
